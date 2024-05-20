01e4b46110afdf889677fdf879ab39af12dbbb77
Adrian Pita <pitabermudez@gmail.com>
2024-05-20 12:20:41 +0200

    El email validator, tremenda fiesta

--- src/app/shared/validators/email-validator.service.ts
+++ src/app/shared/validators/email-validator.service.ts
@@ -6,16 +6,49 @@ import { Observable, delay, of } from 'rxjs';
 
 export class EmailValidatorService implements AsyncValidator {
 
+  // validate(control: AbstractControl<any, any>): Promise<ValidationErrors | null> | Observable<ValidationErrors | null> {
+
+  //   const email = control.value;
+  //   console.log({ email });
+
+  //   return of({
+  //     emailTaken: true
+  //   }).pipe(
+  //     delay(2000)
+  //   )
+
+
   validate(control: AbstractControl<any, any>): Promise<ValidationErrors | null> | Observable<ValidationErrors | null> {
 
     const email = control.value;
-    console.log({ email });
 
-    return of({
-      emailTaken: true
+    const httpCallObservable = new Observable<ValidationErrors | null>((subscriber) => {
+      console.log({ email })
+
+      if (email === 'fernando@google.com') {
+        subscriber.next({ emailTaken: true });
+        subscriber.complete(); //Para que pare el subscriber y no haga nada mas
+      }
+
+      subscriber.next(null);
+      subscriber.complete();
     }).pipe(
-      delay(2000)
-    )
+      delay(3000)
+    );
+
+    return httpCallObservable;
+
+
+    //Como sería si hubiera backend y llamando al servidor
+    // return this.http.get<any[]>(`http://localhost:3000/users?q=${email}`
+    //   .pipe(
+    //     map(resp => {
+    //       return (resp.length === 0)
+    //         ? null
+    //         : { emailTaken: true }
+    //     })
+    //   )
+    // );
 
   }
 
