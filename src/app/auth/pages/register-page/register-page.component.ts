301353f52341600ded54beff087d8c82f84073fb
Adrian Pita <pitabermudez@gmail.com>
2024-05-20 11:59:40 +0200

    Nuevo servicio con un validator asincrono para el email.

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ValidatorsService } from '../../../shared/service/validators.service';
+import { EmailValidatorService } from '../../../shared/validators/email-validator.service';
 
 
 @Component({
@@ -13,11 +14,13 @@ export class RegisterPageComponent {
   constructor(
     private fb: FormBuilder,
     private validatorService: ValidatorsService
+
   ) { }
 
   public myForm: FormGroup = this.fb.group({
     name: ['', [Validators.required, Validators.pattern(this.validatorService.firstNameAndLastnamePattern)]],
-    email: ['', [Validators.required, Validators.pattern(this.validatorService.emailPattern)]],
+    email: ['', [Validators.required, Validators.pattern(this.validatorService.emailPattern)],
+      [new EmailValidatorService()]],
     username: ['', [Validators.required, this.validatorService.cantBeStrider]],
     password: ['', [Validators.required, Validators.minLength(6)]],
     password2: ['', [Validators.required]]
---
+++ src/app/shared/validators/email-validator.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { AbstractControl, AsyncValidator, ValidationErrors } from '@angular/forms';
+import { Observable, delay, of } from 'rxjs';
+
+@Injectable({ providedIn: 'root' })
+
+export class EmailValidatorService implements AsyncValidator {
+
+  validate(control: AbstractControl<any, any>): Promise<ValidationErrors | null> | Observable<ValidationErrors | null> {
+
+    const email = control.value;
+    console.log({ email });
+
+    return of({
+      emailTaken: true
+    }).pipe(
+      delay(2000)
+    )
+
+  }
+
+
+
+
+
+}
