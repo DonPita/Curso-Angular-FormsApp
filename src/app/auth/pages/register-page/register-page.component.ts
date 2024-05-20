0a050184c8f4e94d5047117a5a9435fbc2c719cd
Adrian Pita <pitabermudez@gmail.com>
2024-05-20 10:26:03 +0200

    Validacion de email y nombre

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { cantBeStrider } from '../../../shared/validators/validators';
+import * as customValidators from '../../../shared/validators/validators';
+
 
 @Component({
   templateUrl: './register-page.component.html',
@@ -10,9 +11,9 @@ import { cantBeStrider } from '../../../shared/validators/validators';
 export class RegisterPageComponent {
 
   public myForm: FormGroup = this.fb.group({
-    name: ['', [Validators.required]],
-    email: ['', [Validators.required]],
-    username: ['', [Validators.required, cantBeStrider]],
+    name: ['', [Validators.required, Validators.pattern(customValidators.firstNameAndLastnamePattern)]],
+    email: ['', [Validators.required, Validators.pattern(customValidators.emailPattern)]],
+    username: ['', [Validators.required, customValidators.cantBeStrider]],
     password: ['', [Validators.required, Validators.minLength(6)]],
     password2: ['', [Validators.required]]
   })
@@ -23,7 +24,7 @@ export class RegisterPageComponent {
     this.myForm.markAllAsTouched();
   }
 
-  isValidField ( field: string) {
+  isValidField(field: string) {
     //TODO: Obtener validacion desde un servicio.
   }
 
