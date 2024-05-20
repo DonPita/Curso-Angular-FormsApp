a75b42c5d72b232a9a85f7221b57ebd6f0cf8360
Adrian Pita <pitabermudez@gmail.com>
2024-05-20 09:40:56 +0200

    Validaciones en switches-page y empezando register-page

--- src/app/reactive/pages/switches-page/switches-page.component.ts
+++ src/app/reactive/pages/switches-page/switches-page.component.ts
@@ -1,9 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './switches-page.component.html',
   styles: ``
 })
-export class SwitchesPageComponent {
+
+export class SwitchesPageComponent implements OnInit {
+
+  public myForm: FormGroup = this.fb.group({
+    gender: ['M', Validators.required],
+    wantNotification: [true, Validators.required],
+    termsAndConditions: [false, Validators.requiredTrue]
+  });
+
+  constructor(private fb: FormBuilder) { }
+
+  public person = {
+    gender: 'M',
+    wantNotificiation: false,
+  }
+
+  ngOnInit(): void {
+    this.myForm.reset(this.person);
+  }
+
+
+  //ngSubtmit
+  onSave() {
+    if (this.myForm.invalid) {
+      this.myForm.markAllAsTouched();
+      return;
+    }
+
+    const { termsAndConditions, ...newPerson} = this.myForm.value;
+
+    this.person = newPerson;
+
+
+  }
+
+  isValidField(field: string): boolean | null {
+    return this.myForm.controls[field].errors
+      && this.myForm.controls[field].touched;
+  }
+
+
+
+
 
 }
